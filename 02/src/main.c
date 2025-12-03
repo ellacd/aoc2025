f01c9bc2ddb3885c71197e509cb5e80222a36376
Ella Duncan <ella.lr.duncan@gmail.com>
2025-12-03 09:13:21 -0800

    Completed day 2

--- 02/src/main.c
+++ 02/src/main.c
@@ -3,7 +3,9 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define LITTLE_BUFFER_SIZE 32
 #define BUFFER_SIZE 128
 #define RANGE_SEP ','
 #define ID_SEP '-'
@@ -11,6 +13,7 @@
 uint64 ulog10(uint64 n);
 uint64 upow(uint64 b, uint64 n);
 uint64 count_invalid(uint64 lo, uint64 hi);
+boolean has_pattern(const char *s, const char *pattern);
 
 int main(void)
 {
@@ -43,25 +46,30 @@ int main(void)
     }
 
     printf("ans: %lu\n", ans);
+
+    free(buf);
+    fclose(f);
 }
 
 uint64 count_invalid(uint64 lo, uint64 hi)
 {
+    uint32 i;
+    uint64 len;
     uint64 ans = 0;
-    uint64 magnitude;
-    uint64 l_half, r_half;
+    char s[LITTLE_BUFFER_SIZE];
+    char pattern[LITTLE_BUFFER_SIZE];
 
     while(lo <= hi) {
-        magnitude = ulog10(lo);
-        if (magnitude % 2 != 0) {
-            lo++;
-            continue;
-        }
-        l_half = lo / upow(10, magnitude / 2);
-        r_half = lo % upow(10, magnitude / 2);
-        if (l_half == r_half) {
-            ans += lo;
-            printf("found: %lu\n", lo);
+        len = ulog10(lo);
+        snprintf(s, LITTLE_BUFFER_SIZE, "%lu", lo);
+        for (i = 1; i <= len / 2; i++) {
+            if (len % i != 0) continue;
+            strcpy(pattern, s);
+            pattern[i] = '\0';
+            if (has_pattern(s, pattern)) {
+                ans += lo;
+                break;
+            }
         }
         lo++;
     }
@@ -90,4 +98,23 @@ uint64 upow(uint64 b, uint64 n)
     }
     return ans;
 
+}
+
+boolean has_pattern(const char *s, const char *pattern)
+{
+    uint32 i;
+    uint32 repeats;
+    uint32 len_s = (uint32) strlen(s);
+    uint32 len_pattern = (uint32) strlen(pattern);
+    char substr[LITTLE_BUFFER_SIZE];
+    if (len_s % len_pattern != 0) return FALSE;
+
+    repeats = len_s / len_pattern;
+    for (i = 0; i < repeats; i++) {
+        strcpy(substr, s + i * len_pattern);
+        substr[len_pattern] = '\0';
+        if(strstr(substr, pattern) == NULL) return FALSE;
+    }
+    printf("s: %s\n\thas pattern: %s\n", s, pattern);
+    return TRUE;
 }
